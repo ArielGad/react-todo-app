2c494ba08687bb28919680f96f1343199b425732
ArielGad <ariel.gad4@gmail.com>
2019-02-25 21:50:08 +0200

    add ability to to delete all todo's

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import Todos from './Todos';
 
 class App extends Component {
-  render() {
+    state = {
+        todos: [
+            {content:"Eat food", id:1},
+            {content:"Clean dishes", id:2},
+            {content:"Go to sleep", id:3}
+            ]
+    }
+
+    deleteTodo = (id) => {
+        const newList = this.state.todos.filter((todo) =>{
+            return todo.id !== id
+        })
+
+        this.setState({todos:newList})
+}
+
+    render() {
     return (
-      <div className="App">
+      <div className="todo-app container">
+      <h1 className="center blue-text">Todo's</h1>
+      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
-  }
+    }
+
 }
 
 export default App;
